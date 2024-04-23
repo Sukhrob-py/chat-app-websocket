9ac2da688eeba116431d75cbca48db81f4e9bb1a
sukhrob-py <sukhrobovlayev@gmail.com>
2024-04-23 16:59:29 +0500

    edit

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,13 +1,16 @@
 import json
 
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-from asgiref.sync import async_to_sync
+from asgiref.sync import async_to_sync, sync_to_async
+
+from .models import Room, Messages
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
+        await self.send_all_messages(self.room_name)
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
@@ -19,6 +22,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        await self.save_message(message)
 
         #! Send message to room group
         await self.channel_layer.group_send(
@@ -29,6 +33,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def chat_message(self, event):
         message = event["message"]
-
         #! Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
+
+    @sync_to_async
+    def send_all_messages(self, event):
+        messages = Messages.objects.filter(room__name=event)
+        for msg in messages:
+            self.send(text_data=json.dumps({"message": msg.message}))
+
+    @sync_to_async
+    def save_message(self, event):
+        try:
+            room = Room.objects.get(name=self.room_name)
+        except:
+            room = Room.objects.create(name=self.room_name)
+        Messages.objects.create(room=room, message=event)
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from .models import Room, Messages
+
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    rooms = Room.objects.all()
+    return render(request, "index.html", {"rooms": rooms})
+
 
 def room(request, room_name):
-    return render(request, "room.html", {"room_name": room_name})
+    Room.objects.get_or_create(name=room_name)
+    messages = Messages.objects.filter(room__name=room_name)
+    return render(request, "room.html", {"room_name": room_name, "messages": messages})
